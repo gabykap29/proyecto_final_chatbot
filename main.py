a395afe9f06cbdd7ca9ae1c4a0a8d94ac9695e07
GAbriel Acosta <gabriacos@gmail.com>
2024-11-09 21:24:22 -0300

    feat: :zap: Se agregó la configuración para responder en forma streaming o chunks

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain.prompts import PromptTemplate
 
-# Definimos el modelo de llm que vamos a utilizar
-llm = ChatOllama(model="llama3.2")
+# modelo de llm que vamos a utilizar
+llm = ChatOllama(model="llama3.2", streaming=True)  #  `streaming=True` al modelo para obtener la respuesta en partes
 
-# Definimos el path del archivo pdf (ruta relativa en este caso)
+# path del archivo pdf (ruta relativa en este caso)
 file_path = "./overlord.pdf"
 
-# Cargamos el archivo pdf
+# se carga el archivo pdf
 loader = PyMuPDFLoader(file_path)
 
-# Cargamos el contenido del pdf
+# se carga el contenido del pdf
 data_pdf = loader.load()
 
-# Definimos el tamaño de los chunks y el overlap (superposición de los chunks)
+# se define el tamaño de los chunks y el overlap (superposición de los chunks)
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=500)
 
-# Dividimos el contenido del pdf en chunks
+# se divide el contenido del pdf en chunks
 chunks = text_splitter.split_documents(data_pdf)
 
-# Definimos el modelo de embeddings que vamos a utilizar
+# se define el modelo de embeddings que vamos a utilizar
 embed_model = FastEmbedEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-# Definimos el template de la pregunta
+# template de la pregunta
 custom_prompt_template = """Usa la siguiente información para responder a la pregunta del usuario.
 Si la respuesta no se encuentra en dicha información, di que no sabes la respuesta.
 
@@ -35,35 +35,43 @@ Pregunta: {question}
 Solo devuelve la respuesta útil a continuación y nada más. Responde siempre en español:
 """
 
-# Definimos el prompt template para la pregunta
+# se define el prompt template para la pregunta
 prompt = PromptTemplate(
     template=custom_prompt_template,
     input_variables=['context', 'question']
 )
 
-# Función que busca en los chunks y pasa el contexto al modelo
+# funciona para buscar en los chunks y pasar el contexto al modelo
 def answer_question(question: str, chunks: list):
-    # Unimos todos los chunks como contexto. Accedemos al contenido del documento a través del atributo 'page_content'.
+    # se unen todos los chunks como contexto. Accedemos al contenido del documento a través del atributo 'page_content'.
     context = "\n".join([chunk.page_content for chunk in chunks]) 
     
-    # Creamos el prompt con el contexto y la pregunta
+    # se crea el prompt con el contexto y la pregunta
     formatted_prompt = prompt.format(context=context, question=question)
     
     # Reformateamos el mensaje para cumplir con los requisitos de Ollama
     messages = [
-        {"role": "system", "content": "Eres un asistente de inteligencia artificial."},
+        {"role": "system", "content": "Eres un asistente de inteligencia artificial que solo responderá a preguntas sobre el contexto, y si te insultan o dicen palabras como 'inutil' o 'no sirves para nada', responde 'Nunca me digas eso emoji_carita_llorando'"},
         {"role": "user", "content": formatted_prompt},
     ]
-    # Invocamos al modelo
-    response = llm.invoke(messages)  
     
-    return response
+    # Invocamos al modelo con el parámetro `streaming=True` para obtener la respuesta en partes
+    response = llm.stream(messages)  # Usamos `stream` en lugar de `invoke`
+    
+    # Procesamos y mostramos las respuestas conforme llegan
+    for chunk in response:
+        # Imprimimos los fragmentos de la respuesta a medida que llegan
+        print(chunk.content, end="", flush=True)
 
-# Realizamos la pregunta al modelo
-quest = input("Ingrese su pregunta: ")
-
-# Obtenemos la respuesta
-resp = answer_question(quest, chunks)
-
-# Imprimimos la respuesta
-print(resp.content)
+#hacemos un while para que el usuario pueda hacer varias preguntas
+while True:
+    # Preguntamos al usuario
+    question = input("&: ")
+    
+    # si el usuario escribe 'salir', salimos del bucle
+    if question == 'salir':
+        break
+    
+    # función para responder a la pregunta
+    answer_question(question, chunks)
+    print()
